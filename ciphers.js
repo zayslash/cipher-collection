e48c46a14a01539263db6a87edf83c8a94e37b2d
zayslash <zanif.sandy@student.mec.cuny.edu>
2018-09-04 19:44:18 -0400

    methods

--- ciphers.js
+++ ciphers.js
@@ -2,8 +2,26 @@ var textbox = document.getElementById("text-box");
 var submit = document.getElementById("submit");
 var result = document.getElementById("results");
 var Title = document.getElementById("title");
+var DOM_shift = document.getElementById("shift");
+var method = document.getElementById("operation");
+var method_specification = "Encode";
+
 var menuPosition = ["Caesar Cipher"];
 let position = 0;
+
+console.log(method.innerHTML);
+method.onclick = function(){
+  if (method.innerHTML === "Encode"){
+    method.innerHTML = "Decode";
+    method_specification = "Encode"
+
+        submit.value = "Encode"
+  }else{
+    method.innerHTML = "Encode";
+    submit.value = "Decode"
+    method_specification = "Decode"
+  }
+}
 
 function ascii (a) { return a.charCodeAt(0); }
 
@@ -11,26 +29,43 @@ function codeToAscii(number){
     return String.fromCharCode(number);
 }
 
-
-var caesarCipher = function(text, shift){
-  var citext = "";
+var caesarCipher = function(text, shift,method){
+var citext = "";
+  if(method === "Encode"){
    for (let i = 0; i < text.length; i++){
+      if(text[i] === " "){
+        citext += " ";
+        //i++;
+      }
+      else{
        citext += codeToAscii(ascii(text[i]) + shift);
        console.log(codeToAscii((ascii(text[i]))));
+     }
    }
 return citext;
-
+} else{
+for (let i = 0; i < text.length; i++){
+   if(text[i] === " "){
+     citext += " ";
+     //i++;
+   }
+   else{
+    citext += codeToAscii(ascii(text[i]) - shift);
+    console.log(codeToAscii((ascii(text[i]))));
+  }
+}
+return citext;
+}
 }
 
 
 submit.onclick = function(){
 let textdata = textbox.value;
-result.innerHTML = caesarCipher(textdata,2);
+result.innerHTML = caesarCipher(textdata,parseInt(DOM_shift.value), method_specification);
 }
 
 function loaders(){
    Title.innerHTML = menuPosition[position];
-
 
 
 }
